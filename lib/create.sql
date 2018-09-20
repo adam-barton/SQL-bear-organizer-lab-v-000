e3c4e395e3378fa369597a45c0440f95de097244
Adam Barton <adam.barton333@gmail.com>
2018-09-20 04:34:42 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,7 @@
 CREATE TABLE bears (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  age INTEGER,
-  gender TEXT,
-  color TEXT,
-  temperament TEXT,
-  alive BOOLEAN,
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    breed TEXT,
+    owner_id INTEGER
 );
