3140b359199f0f814e5732dadde7323859715d2c
Adam Barton <adam.barton333@gmail.com>
2018-09-20 04:43:53 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name) VALUES ("Mr. Chocolate");
-INSERT INTO bears (name) VALUES ("Rowdy");
-INSERT INTO bears (name) VALUES ("Tabitha");
-INSERT INTO bears (name) VALUES ("Sergeant Brown");
-INSERT INTO bears (name) VALUES ("Melissa");
-INSERT INTO bears (name) VALUES ("Grinch");
-INSERT INTO bears (name) VALUES ("Wendy");
-INSERT INTO bears (name, temperament) VALUES (NULL, "Agressive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, "Agressive");
